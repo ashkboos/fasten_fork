5cf17c7f30a966981934263cb638f981ad96aff1
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2021-06-20 09:41:24 +0200

    PR-based test

--- core/src/main/java/eu/fasten/core/search/SeedTestPR.java
+++ core/src/main/java/eu/fasten/core/search/SeedTestPR.java
@@ -18,10 +18,17 @@
 
 package eu.fasten.core.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.jgrapht.Graph;
+import org.jgrapht.alg.scoring.PageRank;
 import org.jgrapht.graph.EdgeReversedGraph;
 import org.jgrapht.traverse.ClosestFirstIterator;
 import org.jgrapht.traverse.DepthFirstIterator;
@@ -30,6 +37,7 @@ import org.jooq.conf.ParseUnknownFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.primitives.Doubles;
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPResult;
 import com.martiansoftware.jsap.Parameter;
@@ -48,8 +56,8 @@ import it.unimi.dsi.lang.ObjectParser;
  * Computes statistics for seeding.
  */
 
-public class SeedTest {
-	private static final Logger LOGGER = LoggerFactory.getLogger(SeedTest.class);
+public class SeedTestPR {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SeedTestPR.class);
 
 	/** The handle to the Postgres metadata database. */
 	private final DSLContext context;
@@ -73,7 +81,7 @@ public class SeedTest {
 	 * @param scorer an {@link ObjectParser} specification providing a scorer; if {@code null}, a
 	 *            {@link TrivialScorer} will be used instead.
 	 */
-	public SeedTest(final String jdbcURI, final String database, final String rocksDb, final String resolverGraph, final String scorer) throws Exception {
+	public SeedTestPR(final String jdbcURI, final String database, final String rocksDb, final String resolverGraph, final String scorer) throws Exception {
 		this(PostgresConnector.getDSLContext(jdbcURI, database, false), null, resolverGraph, scorer == null ? TrivialScorer.getInstance() : ObjectParser.fromSpec(scorer, Scorer.class));
 	}
 
@@ -88,7 +96,7 @@ public class SeedTest {
 	 *            {@link TrivialScorer} will be used instead.
 	 */
 
-	public SeedTest(final DSLContext context, final RocksDao rocksDao, final String resolverGraph, final Scorer scorer) throws Exception {
+	public SeedTestPR(final DSLContext context, final RocksDao rocksDao, final String resolverGraph, final Scorer scorer) throws Exception {
 		this.context = context;
 		this.rocksDao = rocksDao;
 		resolver = new GraphMavenResolver();
@@ -96,15 +104,14 @@ public class SeedTest {
 		resolver.setIgnoreMissing(true);
 	}
 
-
 	public static void main(final String args[]) throws Exception {
-		final SimpleJSAP jsap = new SimpleJSAP(SeedTest.class.getName(), "Prints statistics on visits on the dependency graph using a given list of popular products.", new Parameter[] {
+		final SimpleJSAP jsap = new SimpleJSAP(SeedTestPR.class.getName(), "Prints statistics on visits on the dependency graph using PageRank to locate important products.", new Parameter[] {
 				new UnflaggedOption("jdbcURI", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The JDBC URI."),
 				new UnflaggedOption("database", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The database name."),
 				new UnflaggedOption("rocksDb", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The path to the RocksDB database of revision call graphs."),
 				new UnflaggedOption("resolverGraph", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The path to a resolver graph (will be created if it does not exist)."),
-				new UnflaggedOption("radius", JSAP.INTEGER_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "Backward ball radius."),
-				new UnflaggedOption("seedProducts", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, JSAP.GREEDY, "Seed products."), });
+				new UnflaggedOption("n", JSAP.INTEGER_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "Number of seed products."),
+				new UnflaggedOption("radius", JSAP.INTEGER_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "Backward ball radius."), });
 
 		final JSAPResult jsapResult = jsap.parse(args);
 		if (jsap.messagePrinted()) System.exit(1);
@@ -115,17 +122,35 @@ public class SeedTest {
 		final String rocksDb = jsapResult.getString("rocksDB");
 		final String resolverGraph = jsapResult.getString("resolverGraph");
 		final int radius = jsapResult.getInt("radius");
-		final String[] allSeedProducts = jsapResult.getStringArray("seedProducts");
+		final int n = jsapResult.getInt("n");
 
-		final SeedTest test = new SeedTest(jdbcURI, database, rocksDb, resolverGraph, null);
+		final SeedTestPR searchEngine = new SeedTestPR(jdbcURI, database, rocksDb, resolverGraph, null);
 
-		final DSLContext context = test.context;
+		final DSLContext context = searchEngine.context;
 		context.settings().withParseUnknownFunctions(ParseUnknownFunctions.IGNORE);
 
 		final Graph<Revision, DependencyEdge> graph = GraphMavenResolver.dependencyGraph;
 
+		LOGGER.info("Computing PageRank...");
+
+		final PageRank<Revision, DependencyEdge> pageRank = new PageRank<>(graph, 0.85, 20);
+		final Map<Revision, Double> scores = pageRank.getScores();
+
+		LOGGER.info("Sorting revisions by PageRank...");
+
+		final Revision[] revs = graph.vertexSet().toArray(new Revision[0]);
+		Arrays.sort(revs, (x, y) -> Doubles.compare(scores.get(y).doubleValue(), scores.get(x).doubleValue()));
+
+		final LinkedHashSet<String> products = new LinkedHashSet<>();
+		for(int i = 0; products.size() < n; i++) {
+			products.add(revs[i].groupId + ":" + revs[i].artifactId);
+
+		final String[] allSeedProducts = products.toArray(new String[n]);
+
+		LOGGER.info("Seed products: " + Arrays.toString(allSeedProducts));
+
 		for (int r = 1; r <= radius; r++) {
-			for (int p = 1; p < allSeedProducts.length; p++) {
+			for (int p = 1; p < n; p++) {
 				final Set<String> seedProducts = new ObjectOpenHashSet<>(allSeedProducts, 0, p);
 				final Set<Revision> seeds = new HashSet<>();
 
